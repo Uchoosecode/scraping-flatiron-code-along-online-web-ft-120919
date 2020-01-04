3f629e983612c5cd8468fed4175f243078a3d3a8
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-04 04:42:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,13 +10,16 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-   
+    doc.css(".post").each{|post| 
+      course = Course.new 
+      course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text}
+   binding.pry
   end
   
   def get_courses
-    title = doc.css(".post").first.css("h2").text
-    schedule = doc.css(".post").first.css(".date").text
-    description = doc.css(".post").first.css("p").text
+    
     
     
   end
