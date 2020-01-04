11b0a86d45b1d8743343694d227b3bcab2081446
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-04 03:46:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  
+  def get_page
+    
+  end
+  
+  def get_courses
+    
+    
+  end
+  
+  def make_courses
+    
+  end
   
   def print_courses
     self.make_courses
@@ -15,6 +30,10 @@ class Scraper
       end
     end
   end
+ 
+  
+  
+  
   
 end
 
