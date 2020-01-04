9964433747ec1d7c02235e1ad9535b6c955fb1d1
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-04 03:57:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   
   
   def get_page
-    doc = Nokogiri::HTML(open("http://http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
    
   end
